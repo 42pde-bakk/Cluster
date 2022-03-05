6a6da36d9263f84d49cb816a441e32c41b2095cd
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-03-05 22:53:02 +0100

    partly start of gameloop

--- srcs/main.c
+++ srcs/main.c
@@ -1,11 +1,37 @@
 #include "cluster.h"
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-	init_field();
+	char	command[3];
+	char	col1;
+	char	col2;
+	bool	player = 0;
 
-	print_grid_terminal('a', 'b');
-	printf("\n");
+	init_field();
+	while (1)
+	{
+		//who's turn is it? Player 0 has colors red and yellow, player 1 has colors green and blue. This whould be done by a function that gives 4 different options later : r r || r y || y r || y y etc
+		if (player == 0)
+		{
+			col1 = 'r';
+			col2 = 'y';
+		}
+		else
+		{
+			col1 = 'b';
+			col2 = 'g';
+		}
+		//read command
+		scanf("%s", command);
+		//if the command given starts with A, B or R, see it as a valid command and (later: update the field), print the field and change the turn to the other player
+		if (strncmp(command, "A", 1) == 0 || strncmp(command, "B", 1) == 0 || strncmp(command, "R", 1) == 0)
+		{
+			print_grid_terminal(col1, col2);
+			player = !player;
+		}
+		
+	}
 	// print to show it works
 
 //	while (game_alive()) {
