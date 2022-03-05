d665c2ee172892e1744521c92e47b2289cdaf8e2
pde-bakk <pde-bakk@student.codam.nl>
2022-03-05 19:52:27 +0100

    added comments to show thought process of game loop

--- srcs/main.c
+++ srcs/main.c
@@ -3,5 +3,12 @@
 
 int main() {
 	init_field();
+	// print to show it works
+
+//	while (game_alive()) {
+//		ask_input();
+//		calculate_game_state();
+//		print_or_return_output();
+//	}
 	return 0;
 }
