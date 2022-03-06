01d3a46c43a969300865ef7fd0eb0f3e6b6a2533
pde-bakk <pde-bakk@student.codam.nl>
2022-03-06 16:35:56 +0100

    altered print func

--- srcs/output/print_grid_terminal.c
+++ srcs/output/print_grid_terminal.c
@@ -15,11 +15,11 @@
 #define GRY   "\x1B[90m"
 #define RESET "\x1B[0m"
 
-void    print_grid_terminal(char col1, char col2)
+void    print_grid_terminal()
 {
     FILE *grid = fopen("maps/grid5.txt", "r");
     get_indices(get_opposite_direction(g_field.gravity));
-    char	*items[5] = { WHT"{}"RESET, BLU"##"RESET, RED"@@"RESET, GRN"$$"RESET, YEL"&&"RESET };
+    char	*items[5] = { WHT"{}"RESET, RED"##"RESET, YEL"@@"RESET, BLU"$$"RESET, CYN"&&"RESET };
     char    c;
     int     i = 0;
     
@@ -37,31 +37,11 @@ void    print_grid_terminal(char col1, char col2)
         }
         else if (c == '|' || c == 'v')
             printf(GRY "%c", c);
-        else if (c == 'A')
-        {
-            if (col1 == 'r')
-                printf(RED "%c", c);
-            else if (col1 == 'y')
-                printf(YEL "%c", c);
-            else if (col1 == 'g')
-                printf(GRN "%c", c);
-            else if (col1 == 'b')
-                printf(BLU "%c", c);
-            else
-                printf(WHT "%c", c);
+        else if (c == 'A') {
+            printf(RED "%c" RESET, c);
         }
-        else if (c == 'B')
-        {
-            if (col2 == 'r')
-                printf(RED "%c", c);
-            else if (col2 == 'y')
-                printf(YEL "%c", c);
-            else if (col2 == 'g')
-                printf(GRN "%c", c);
-            else if (col2 == 'b')
-                printf(BLU "%c", c);
-            else
-                printf(WHT "%c", c);
+        else if (c == 'B') {
+            printf(BLU "%c" RESET, c);
         }
         else
             printf(WHT "%c", c);
