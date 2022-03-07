bbfc5ed5dc59be5e70e7df2165d86cc788877787
Jorien Aberkrom <jaberkro@student.codam.nl>
2022-03-07 15:05:20 +0100

    cleaned up a bit

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-	//char	command[3];
-	//int		nb;
-
+int main(void) {
 	bool	player_turn = 0;
 	int		ringsize = 5;
 
@@ -15,37 +12,17 @@ int main() {
 	while (1)
 	{
 		printf("turn: %d\n", player_turn);
-		play_turn(player_turn, players);
-		//check of gewonnen
-		player_turn = !player_turn;
-		//read command
-		//print_grid_terminal(players->p[player_turn].col1, players->p[player_turn].col2);
-		// bzero(command, sizeof(command));
-		// scanf("%c%d", command, &nb);
-		// //if the command given starts with A, B or R, see it as a valid command and (later: update the field), print the field and change the turn to the other player
-		// if (strncmp(command, "D", 1) == 0) {
-		// 	get_drop_tile(nb, player *2 + 1);
-		// 	// player 0 will drop colour 1 or 2 (0 = empty)
-		// 	// player 1 will drop colour 3 or 4
-		// }
-		// else if (strncmp(command, "R", 1) == 0) {
-		// 	printf("in here!\n");
-		// 	rotate_field(nb);
-		// }
-		// player = !player;
-		// if (strncmp(command, "A", 1) == 0 || strncmp(command, "B", 1) == 0 || strncmp(command, "R", 1) == 0)
-		// {
-		// 	print_grid_terminal(col1, col2);
-		// 	player = !player;
-		// }
-		
-	}
-	// print to show it works
 
-//	while (game_alive()) {
-//		ask_input();
-//		calculate_game_state();
-//		print_or_return_output();
-//	}
+		//print grid to show options for player
+    	print_grid_terminal(players->p[turn].col1, players->p[turn].col2);
+
+		//player plays their turn
+		play_turn(player_turn, players);
+
+		//check if there is a winner
+
+		//change turn to the next player
+		player_turn = !player_turn;	
+	}
 	return 0;
 }
--- srcs/player/play_turn.c
+++ srcs/player/play_turn.c
@@ -6,7 +6,7 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/07 12:24:29 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/03/07 14:59:55 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/03/07 15:02:59 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,13 @@ void play_turn(bool turn, t_players *players)
     //print grid to show options for player
     print_grid_terminal(players->p[turn].col1, players->p[turn].col2);
 
-    // let the player choose the input
+    // player give input
     bzero(command, 3000);
     while ((command[0] != 'A' && command[0] != 'B' && command[0] != 'R' ) || (command[1] - 48 < 0 && command[1] - 48 > 9))
         scanf("%s", command);
     //printf("command=%s, turn = %d, colours: %d, %d\n", command, turn, players->p[turn].col1, players->p[turn].col2);
+
+    // work out player input
     if (command[0] == 'A')
     {
         get_drop_tile(command[1] - 48, players->p[turn].col1);
@@ -43,7 +45,4 @@ void play_turn(bool turn, t_players *players)
         printf("let's rotate!\n");
         rotate_field(command[1] - 48);
     }
-
-    // show result
-    //print_grid_terminal(players->p[turn].col1, players->p[turn].col2);
 }
