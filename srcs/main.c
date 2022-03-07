6a8b2511698762d81b3b2d2547fdaf1918c8034b
pde-bakk <pde-bakk@student.codam.nl>
2022-03-07 23:05:02 +0100

    cleanup on isle main

--- srcs/main.c
+++ srcs/main.c
@@ -5,40 +5,44 @@
 #include <string.h>
 
 int main(int argc, char **argv) {
+	size_t	turn = 0;
 	int		ringsize = 5;
 	int 	winner = -1;
 	int		winning_row_length = 4;
 
 	(void)argc;
+	init_field();
 	t_players *players = init_players(ringsize);
 	for (int p = 0; p < MAX_PLAYER && argv[p + 1]; ++p) {
 		init_player_process(argv[p + 1], &players->p[p]);
+		send_game_info(&players->p[p]);
 	}
-	init_field();
 	while (winner == -1) {
 		//print grid to show options for player
-		for (int p = 0; p < MAX_PLAYER; ++p) {
-			dprintf(2, "p = %d\n", p);
+		for (int i = 0; i < MAX_PLAYER; ++i) {
+			t_player	*player = &players->p[i];
 			int winning_colour = 0;
 			int col1, col2;
 
-			generate_random_colours(&players->p[p], &col1, &col2);
+			generate_random_colours(player, &col1, &col2);
+			if (player->pid) {
+				send_turn_info(player, turn, col1, col2);
+			}
 			print_grid_terminal(col1, col2);
-			printf("col1=%d, col2=%d\n", col1, col2);
 
 			//player plays their turn
-			t_move move = player_request_input(&players->p[p]);
+			t_move move = player_request_input(player);
+			print_move(2, &move);
 			if (move.type == ALPHA || move.type == BETA) {
 				// update inventory
-				update_inventory(&players->p[p], (int)move.type);
-				move.colour = players->p[p].col[move.type];
+				update_inventory(player, (int)move.type);
+				move.colour = player->col[move.type];
 			}
 			const t_tile *played_tile = execute_move(&move);
 			if ((move.type == ALPHA || move.type == BETA) && played_tile)
 				winning_colour = win_check_this_tile(played_tile, winning_row_length);
 			else if (move.type == ROTATE)
 				winning_colour = win_check_all_tiles(winning_row_length);
-			printf("after winning check\n");
 			if (winning_colour) {
 				// 1 if winning_colour is 3 or 4
 				// 0 if winning_colour is 1 or 2
@@ -47,6 +51,7 @@ int main(int argc, char **argv) {
 				break;
 			}
 		}
+		turn += 1;
 	}
 	return 0;
 }
