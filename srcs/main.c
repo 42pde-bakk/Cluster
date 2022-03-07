6225eec7b6c0508a8809fc35d838b712844745d0
CookieMonsterXL <88137591+CookieMonsterXL@users.noreply.github.com>
2022-03-07 15:47:45 +0100

    ckeck win update

--- srcs/main.c
+++ srcs/main.c
@@ -6,6 +6,9 @@
 int main(void) {
 	bool	player_turn = 0;
 	int		ringsize = 5;
+	t_tile	*played_tile;
+	int		winning_colour;
+	int		winning_row_length;
 
 	t_players *players = init_players(ringsize);
 	init_field();
@@ -15,9 +18,13 @@ int main(void) {
     	print_grid_terminal(players->p[player_turn].col1, players->p[player_turn].col2);
 
 		//player plays their turn
-		play_turn(player_turn, players);
-
-		//check if there is a winner
+		played_tile = play_turn(player_turn, players);
+		if (played_tile == 0)
+			winning_colour = win_check_all_tiles(winning_row_length)
+		else
+			winning_colour = win_check_this_tile(played_tile, winning_row_length);
+		if (winning_colour)
+			return (player of winning_colour);
 
 		//change turn to the next player
 		player_turn = !player_turn;	
