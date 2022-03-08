99e1d76a32d1a14bff80ab651f84f565d768b187
pde-bakk <pde-bakk@student.codam.nl>
2022-03-08 11:21:00 +0100

    added basic bot with random moveset to test our process handling

--- bot.c
+++ bot.c
@@ -4,22 +4,30 @@
 #include <stdbool.h>
 #include <sys/wait.h>
 #include "move.h"
+#include <fcntl.h>
 
-void	do_move() {
+void	do_move(const int fd) {
 	static const char* moves[] = {"A", "B", "R"};
 	int move_type = rand() % 3;
 	int move_value;
 	if (move_type == ROTATE)
 		move_value = rand() % 5 + 1;
-	else
+	else {
 		move_value = rand() % 9 + 1;
+	}
 	printf("%s %d\n", moves[move_type], move_value);
 	fflush(stdout);
 	dprintf(2, "MOVE: %s %d\n", moves[move_type], move_value);
+	dprintf(fd, "%s %d\n", moves[move_type], move_value);
 }
 
 int main()
 {
+	int fd = open("bot.log", O_WRONLY | O_TRUNC | O_CREAT, S_IRWXU | S_IRUSR);
+	if (fd == -1) {
+		dprintf(2, "opening bot.log failed\n");
+		return (1);
+	}
 	// 37
 	int nb_tiles;
 	scanf("%d", &nb_tiles);
@@ -37,30 +45,30 @@ int main()
 		scanf("%d %d %d %d %d %d %d", &index, &neigh_0, &neigh_1, &neigh_2, &neigh_3, &neigh_4, &neigh_5);
 	}
 
-	dprintf(2, "bot: after initial info\n");
 	// game loop
 	while (1) {
 		// the game lasts 24 days: 0-23
 		int turn;
 		// the current amount of played tiles
 		int nb_played_tiles = 0;
+		int	col1; // playable tile A
+		int	col2; // playable tile B
 		scanf("%d %d", &turn, &nb_played_tiles);
-		for (int i = 0; i < turn; i++) {
+		scanf("%d %d", &col1, &col2);
+		for (int i = 0; i < nb_played_tiles; i++) {
 			// location of this tree
 			int cell_index;
 			// size of this tree: 0-3
 			int colour;
 			// 1 if this is your tree
 			int _is_mine;
-			int	col1;
-			int	col2;
-			scanf("%d %d %d %d %d", &cell_index, &colour, &_is_mine, &col1, &col2);
+			scanf("%d %d %d", &cell_index, &colour, &_is_mine);
 		}
 		// Write an action using printf(). DON'T FORGET THE TRAILING \n
 		// To debug: fprintf(stderr, "Debug messages...\n");
 
 		// ALPHA cellIdx | BETA cellIdx | ROTATE amount
-		do_move();
+		do_move(fd);
 	}
 	return 0;
 }
