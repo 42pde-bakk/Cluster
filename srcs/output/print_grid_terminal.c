d00894f1571bb32e6047b6dc8b3b8be5b02dfdd5
pde-bakk <pde-bakk@student.codam.nl>
2022-03-09 13:29:01 +0100

    removed maps/ since we are generating it now, and converted indents in print_grid_terminal to tabs

--- srcs/output/print_grid_terminal.c
+++ srcs/output/print_grid_terminal.c
@@ -30,50 +30,50 @@ void    print_grid_terminal(int opta, int optb)
 	if (!grid)
 		grid = generated_map(g_gameinfo.size);
 
-    get_indices(get_opposite_direction(g_field.gravity));
-    static const char	*items_double[6] = { WHT"  "RESET, WHT"{}"RESET, RED"##"RESET, YEL"@@"RESET, GRN"$$"RESET, BLU"&&"RESET };
-    static const char	*items_A[6] = { WHT" "RESET, WHT"A"RESET, RED"#"RESET, YEL"@"RESET, GRN"$"RESET, BLU"&"RESET };
-    static const char	*items_B[6] = { WHT" "RESET, WHT"B"RESET, RED"#"RESET, YEL"@"RESET, GRN"$"RESET, BLU"&"RESET };
-    char    c;
-    int     cell_idx = 0;
+	get_indices(get_opposite_direction(g_field.gravity));
+	static const char	*items_double[6] = { WHT"  "RESET, WHT"{}"RESET, RED"##"RESET, YEL"@@"RESET, GRN"$$"RESET, BLU"&&"RESET };
+	static const char	*items_A[6] = { WHT" "RESET, WHT"A"RESET, RED"#"RESET, YEL"@"RESET, GRN"$"RESET, BLU"&"RESET };
+	static const char	*items_B[6] = { WHT" "RESET, WHT"B"RESET, RED"#"RESET, YEL"@"RESET, GRN"$"RESET, BLU"&"RESET };
+	char    c;
+	int     cell_idx = 0;
 	int		iter = 0;
 
-    system("clear");
+	system("clear");
 
-    while (true)
-    {
+	while (true)
+	{
 		c = grid[iter++];
-        if (c == '.' || c == '\0')
-            break;
-        else if (c == '}')
-            ;
-        else if (c == '{')
-        {
-            printf("%s", items_double[tile_arr[tile_order[cell_idx]]->tile_colour + 1]);
-            cell_idx++;
-        }
-        else if (c == '|' || c == 'v')
-            printf(GRY "%c", c);
-        else if (c == 'A') 
-            printf("%s", items_A[opta + 1]);
-        else if (c == 'B')
-            printf("%s", items_B[optb + 1]);
-        else
-            printf(WHT "%c", c);
-    }
+		if (c == '.' || c == '\0')
+			break;
+		else if (c == '}')
+			;
+		else if (c == '{')
+		{
+			printf("%s", items_double[tile_arr[tile_order[cell_idx]]->tile_colour + 1]);
+			cell_idx++;
+		}
+		else if (c == '|' || c == 'v')
+			printf(GRY "%c", c);
+		else if (c == 'A')
+			printf("%s", items_A[opta + 1]);
+		else if (c == 'B')
+			printf("%s", items_B[optb + 1]);
+		else
+			printf(WHT "%c", c);
+	}
 	printf("\n");
 	int i = 0;
 	while (AB[i])
 	{
-        if (AB[i] == 'A' && opta != -1) {
-            printf("%s", items_A[opta + 1]);
-        }
-        else if (AB[i] == 'B' && optb != -1) {
-            printf("%s", items_B[optb + 1]);
-        }
+		if (AB[i] == 'A' && opta != -1) {
+			printf("%s", items_A[opta + 1]);
+		}
+		else if (AB[i] == 'B' && optb != -1) {
+			printf("%s", items_B[optb + 1]);
+		}
 		else
 			printf("%c", AB[i]);
 		i++;
 	}
-    printf("\n");
+	printf("\n");
 }
